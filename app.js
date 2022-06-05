2693c57d62c77593a1c0d6371edbf5e20f118890
Mohammad Farhan Ali <74434120+mdfarhanali34@users.noreply.github.com>
2022-06-05 19:05:29 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+require('dotenv').config();
+
+const { Client, Intents} = require('discord.js');
+
+const Alpaca = require('@alpacahq/alpaca-trade-api');
+
+const PREFIX = "$";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const { MessageEmbed } = require('discord.js');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+const alpaca = new Alpaca({
+    keyId: 'PK51ZNGFIDEFDCVPMTL1',
+    secretKey: '8fSzlaE7sNsSxHdVd3Bp90CYM1AnV1H4X92yPPq4',
+    paper: true,
+  })
+
+
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+      const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+
+
+        if (CMD_NAME === 'last_price') {
+
+            const trade = await alpaca.getLatestTrade(args[0]);
+            console.log(trade);
+            message.channel.send(JSON.stringify(trade.Price));
+
+
+
+        }else if(CMD_NAME === 'account'){
+
+            alpaca.getAccount().then((account) => {
+
+                // inside a command, event listener, etc.
+                const exampleEmbed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Account Status')
+                .setURL('https://discord.js.org/')
+                .setAuthor({ name: 'Trade Bot', iconURL: 'https://imgur.com/1diwxmy.png', url: 'https://discord.js.org' })
+                .setDescription('This is the description of your account')
+                .setThumbnail('https://imgur.com/1diwxmy.png')
+                .addFields(
+                    { name: 'Cash', value: account.cash },
+                    { name: 'Curency', value: account.currency, inline: true },
+                     //{ name: '\u200B', value: '\u200B' },
+                     { name: 'Portfolio Value', value: account.portfolio_value, inline: true },
+                     { name: 'Buying Power', value: account.buying_power, inline: true },
+                )
+
+                .setImage('https://imgur.com/e2GFQPH.png')
+                .setTimestamp();
+
+                message.channel.send({ embeds: [exampleEmbed] });
+
+
+
+            });
+
+
+        }else if(CMD_NAME === 'buy'){
+
+            alpaca.createOrder({
+                symbol: args[0],
+                qty: args[1],
+                side: 'buy',
+                type: 'market',
+                time_in_force: 'day'
+            });
+
+            const trade = await alpaca.getLatestTrade(args[0]);
+       //     var val = parseInt(args[1])*parseInt(trade);
+            // inside a command, event listener, etc.
+            const example = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Payment Recipt')
+            .setURL('https://discord.js.org/')
+            .setAuthor({ name: 'Trade Bot', iconURL: 'https://imgur.com/1diwxmy.png', url: 'https://discord.js.org' })
+            .setDescription('Thank you for payment')
+            .setThumbnail('https://imgur.com/1diwxmy.png')
+            .addFields(
+            // { name: '\u200B', value: '\u200B' },
+            { name: 'Stock Symbol', value: args[0] },
+            { name: 'Quantity', value: args[1], inline: true },
+            // //{ name: '\u200B', value: '\u200B' },
+           // { name: 'Total Cost', value: val.toString() , inline: true },
+          //  { name: 'Account Balance', value: alpaca.getAccount().buying_power, inline: true },
+            )
+
+            .setImage('https://imgur.com/e2GFQPH.png')
+            .setTimestamp();
+
+            message.channel.send({ embeds: [example] });
+
+        }else if(CMD_NAME === 'news'){
+
+            const news = await alpaca.getNews({ });
+            //Check https://github.com/alpacahq/alpaca-trade-api-js/blob/687e58c8ff03cc70b56fc57844d78ca7801e0f85/lib/resources/datav2/rest_v2.ts#L714
+            console.log(news);
+        }
+    }
+
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
